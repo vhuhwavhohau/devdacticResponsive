bcf24380a8214e3974cb2bec72f0e1aef5200327
vhuhwavhohau <mavhandu3@ymail.com>
2018-08-01 14:41:10 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { HttpClientModule } from '@angular/common/http';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -18,6 +19,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform, AlertController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { HttpClient } from '@angular/common/http';
+import "rxjs/add/operator/map"
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  users: Observable<any>;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController , private httpClient: HttpClient, private plt: Platform, private alertCtrl: AlertController) {
+
+    this.users = this.httpClient.get('https://randomuser.me/api/?results=20')
+   .map(res => res['results'])
 
   }
-
+  checkPlatform() {
+    let alert = this.alertCtrl.create({
+      title: 'Platform',
+      message: 'You are running on: ' + this.plt.platforms(),
+      buttons: ['OK']
+    });
+    alert.present();
+ 
+    if (this.plt.is('cordova')) {
+      // Do Cordova stuff
+    } else {
+      // Do stuff inside the regular browser
+    }
+  }
 }
